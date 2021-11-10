fcc2f1b004890fbd297a2ebda05176b2a1a960a3
Dawid Kraczkowski <dawid.kraczkowski@gmail.com>
2021-11-10 22:47:33 +0100

    Ignore unsupported functionallities

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -7,6 +7,20 @@ import json
 
 schema_test_suits = pathlib.Path(__file__).parent / "test_cases" / "tests"
 
+SKIP_TESTS = [
+    "draft-7 / definitions.json / *",
+    "draft-7 / dependencies.json / multiple dependencies subschema *",
+    "draft-7 / infinite-loop-detection.json / *",
+    "draft-7 / refRemote.json / *",
+    "draft-7 / unknownKeyword.json / *",
+    "draft-7 / ref.json / *",
+    "draft-7 / dependencies.json / dependencies with boolean *",
+    "draft-7 / dependencies.json / dependencies with escaped *",
+    "draft-7 / id.json / *",
+    "draft-7 / propertyNames.json / propertyNames with boolean *",
+    "draft-7 / properties.json / properties, patternProperties, *",
+]
+
 
 def pytest_generate_tests(metafunc):
     parameters = []
@@ -23,10 +37,19 @@ def pytest_generate_tests(metafunc):
             tests = json.load(open(suite))
             for section in tests:
                 for test in section["tests"]:
-                    # if test["description"] != "numbers are unique if mathematically unequal":
-                    #    continue
+                    test_id = f"{version} / {suite.name} / {section['description']} / {test['description']}"
+                    skip = False
+                    for pattern in SKIP_TESTS:
+                        if not pattern.endswith("*"):
+                            if test_id.replace(" ", "") == pattern.replace(" ", ""):
+                                skip = True
+                        else:
+                            if test_id.replace(" ", "").startswith(pattern.replace(" ", "")[0:-1]):
+                                skip = True
+                    if skip:
+                        continue
                     parameters.append(pytest.param(section["schema"], test["data"], test["valid"]))
-                    test_ids.append(f"{version} / {suite.name} / {section['description']} / {test['description']}")
+                    test_ids.append(test_id)
 
     metafunc.parametrize(("schema", "data", "valid"), parameters, ids=test_ids)
 
